16275dc7f66ac0dbaee13d1ddf5ea9fffff0c751
amr8644 <amr8644@gmail.com>
2023-01-30 21:05:13 +0300

    Tried to connect MySQL

--- server/packages/db/config/config.go
+++ server/packages/db/config/config.go
@@ -7,7 +7,7 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 func ConnectToDB()  {
-	db,err := sql.Open("mysql","mysql://root:wMhjYicF281S9mOGG69v@containers-us-west-176.railway.app:5710/railway")
+	db,err := sql.Open("mysql","root:wMhjYicF281S9mOGG69v@containers-us-west-176.railway.app:5710/railway")
 
 	if err != nil {
 		log.Fatal(err.Error())
