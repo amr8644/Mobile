ed7bba3d7a3f77b88818ec567d8bff98b2559b59
amr8644 <amr8644@gmail.com>
2023-01-31 20:44:01 +0300

    Added Flutter

--- server/go.mod
+++ server/go.mod
@@ -4,4 +4,7 @@ go 1.19
 
 require github.com/gorilla/mux v1.8.0
 
-require github.com/go-sql-driver/mysql v1.7.0 // indirect
+require (
+	github.com/go-sql-driver/mysql v1.7.0 // indirect
+	github.com/lib/pq v1.10.7 // indirect
+)
--- server/packages/db/config/config.go
+++ server/packages/db/config/config.go
@@ -4,14 +4,15 @@ import (
 	"database/sql"
 	"log"
 
-	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/lib/pq"
 )
 
 
 func ConnectToDB() (db *sql.DB)  {
 	log.Println("Connecting to MySQL DB...")
-	connstr := "7zvl299euy8qygt8shsn:pscale_pw_UiA7KCCbstkUuns2itrqmjcJHkabDVwOK9ckUtwAq1Y@tcp(ap-southeast.connect.psdb.cloud)/app?tls=true"
-	db, err := sql.Open("mysql", connstr)
+	
+	connstr := "postgresql://postgres:4oAYOA1LxU73TkSQYNih@containers-us-west-166.railway.app:7945/railway"
+	db, err := sql.Open("postgres", connstr)
 
     if err != nil {
         log.Fatalf("failed to connect: %v", err)
